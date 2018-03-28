415b23b73b92563ab19312ae34b8af5f720f1b58
dbeltman <daan.w.beltman@gmail.com>
2018-03-28 12:50:10 -0700

    fresh install script, and dependency script fix

--- install-scripts/dependency_install.sh
+++ install-scripts/dependency_install.sh
@@ -33,7 +33,7 @@ if docker-compose -v 2>/dev/null; then
 else
 	printf "\n===========Docker-compose is not yet installed, will do that now.===========\n"
 	sleep 1
-	sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/$
+	sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 	sudo chmod +x /usr/local/bin/docker-compose
 	if docker-compose -v 2>/dev/null; then
 		printf "\n===========Docker-compose installed succesfully!===========\n"
---
+++ install-scripts/fresh_install.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+sudo add-apt-repository ppa:openjdk-r/ppa -y
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk -y
+sudo ./dependency_install.sh
+
