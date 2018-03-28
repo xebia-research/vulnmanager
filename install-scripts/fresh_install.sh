038518b9e49cac5c7feb666e10a74f89ccb29fff
dbeltman <daan.w.beltman@gmail.com>
2018-03-28 13:22:00 -0700

    install nodejs 8.x (and npm) with fresh_install.sh

--- install-scripts/fresh_install.sh
+++ install-scripts/fresh_install.sh
@@ -4,5 +4,7 @@ sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
+sudo apt install curl -y
 sudo ./dependency_install.sh
-
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
