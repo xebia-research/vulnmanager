f6a3a7c85a74a860af89d3d19424d4317aebb95c
Yorick <y.schellevis@gmail.com>
2018-04-13 15:30:49 +0200

    #51 Added tests for openvas controller and fixed code based on tests

--- src/test/java/com/xebia/vulnmanager/test/OpenvasControllerTest.java
+++ src/test/java/com/xebia/vulnmanager/test/OpenvasControllerTest.java
@@ -1,85 +1,154 @@
 package com.xebia.vulnmanager.test;
 
-import com.xebia.vulnmanager.Main;
+import ch.qos.logback.core.encoder.EchoEncoder;
 import com.xebia.vulnmanager.auth.AuthenticationChecker;
 import com.xebia.vulnmanager.controller.OpenvasController;
-import com.xebia.vulnmanager.controller.TestController;
-import com.xebia.vulnmanager.repositories.OpenvasRepository;
+import com.xebia.vulnmanager.models.openvas.objects.OpenvasReport;
+import com.xebia.vulnmanager.models.openvas.objects.OvResult;
+import com.xebia.vulnmanager.services.OpenvasService;
+import com.xebia.vulnmanager.util.ReportUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Uses main.class because it has the startup of the application
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Main.class)
-@AutoConfigureMockMvc
-@DataJpaTest
-@Transactional(propagation = Propagation.NOT_SUPPORTED)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class OpenvasControllerTest {
 
-    @Autowired
+    private static final String BASE_URL = "http://localhost:8080";
+
     private MockMvc mvc;
 
     @Mock
-    private OpenvasRepository openvasRepo;
+    private OpenvasService openvasService;
 
     @InjectMocks
     private OpenvasController openvasController;
-
-    private final String BASE_URL = "http://localhost:8080";
-
-    @Mock
-    private TestController testController;
 
     @Mock
     private AuthenticationChecker authenticationChecker;
 
     @Before
     public void setup() throws Exception {
-        testController.addTest();
+        // Mock the authentication check to always return true
+        Mockito.when(authenticationChecker.checkCompanyAuthKey(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        Mockito.when(authenticationChecker.checkTeamAndCompany(Mockito.anyString(), Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        Mockito.when(authenticationChecker.checkIfTeamExists(Mockito.any(), Mockito.anyString())).thenReturn(true);
+
+        // Load test report
+        Object parsedDocument = ReportUtil.parseDocument(ReportUtil.getDocumentFromFile(new File("example_logs/openvas.xml")));
+        Object parsedDocument2 = ReportUtil.parseDocument(ReportUtil.getDocumentFromFile(new File("example_logs/openvas.xml")));
+        // Create the reports
+        List<OpenvasReport> reports = new ArrayList<OpenvasReport>();
+        reports.add(ReportUtil.getOpenvasReportFromObject(parsedDocument));
+        reports.add(ReportUtil.getOpenvasReportFromObject(parsedDocument2));
+
+        // Set id to reports en results
+        long id = 1L;
+        for (int i = 0; i < reports.size(); i++) {
+            OpenvasReport report = reports.get(i);
+            // set the id of the report and increment for the next one
+            report.setId(id++);
+            for (OvResult result : report.getResults()) {
+                // set the id of the result
+                result.setId(id++);
+            }
+        }
+
+        // Mock the database service to return a list of valid objects
+        Mockito.when(openvasService.getAllReports()).thenReturn(reports);
+
+        // For any long return null except for other rules
+        // Order of the rules matter
+        Mockito.when(openvasService.getFromRaportByIdResultById(Mockito.anyLong(), Mockito.anyLong())).thenReturn(null);
+        Mockito.when(openvasService.getReportById(Mockito.anyLong())).thenReturn(null);
+        for (int i = 0; i < reports.size(); i++) {
+            OpenvasReport report = reports.get(i);
+            // The other rules
+            Mockito.when(openvasService.getReportById(report.getId())).thenReturn(report);
+            for (OvResult result : report.getResults()) {
+                System.out.println(report.getId() + " -- " + result.getId());
+                Mockito.when(openvasService.getFromRaportByIdResultById(report.getId(), result.getId())).thenReturn(result);
+            }
+        }
+
+
         // MockMvc standalone approach
-        mvc = MockMvcBuilders.standaloneSetup(openvasController, authenticationChecker)
+        mvc = MockMvcBuilders.standaloneSetup(openvasController)
                 .build();
     }
 
     @Test
-    public void openvasWithAuthOk() throws Exception {
-        /*
+    public void getAllReports() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas.json")
                 .header("auth", "testauth"))
                 .andDo(print())
                 .andExpect(status().isOk());
-        */
+
     }
 
     @Test
-    public void openvasWithNoAuth() throws Exception {
-        /*
-        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas.json"))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().json("{\"msg\":\"Auth not correct!\"}"));
-        */
+    public void getReportFromId() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas/1.json")
+                .header("auth", "testauth"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUnknownReportById() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas/3.json")
+                .header("auth", "testauth"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getAllResultsFromReport() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas/1/result.json")
+                .header("auth", "testauth"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getResultByIdFromReport() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas/1/result/3.json")
+                .header("auth", "testauth"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUnknownResultByIdFromReport() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas/1/result/1.json")
+                .header("auth", "testauth"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getUnknownResultByIdFromUnknownReport() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/xebia/vulnmanager/openvas/2/result/1.json")
+                .header("auth", "testauth"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 
 
