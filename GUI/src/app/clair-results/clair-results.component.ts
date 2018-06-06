1b5b66be42c3273a7c8760f6ae6b7c9aebfbbf6b
Jelle Oppenhuis <jelle.oppenhuis@hva.nl>
2018-06-06 13:53:34 +0200

    #91: Changed how information is shown and added sort by

--- GUI/src/app/clair-results/clair-results.component.ts
+++ GUI/src/app/clair-results/clair-results.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {MenuItem} from "primeng/api";
+import {MenuItem, SelectItem} from "primeng/api";
 
 @Component({
   selector: 'app-clair-results',
@@ -15,6 +15,12 @@ export class ClairResultsComponent implements OnInit {
   tags: boolean;
   items: MenuItem[];
 
+  // Sort variables
+  sortOptions: SelectItem[];
+  sortKey: string;
+  sortField: string;
+  sortOrder: number;
+
 
   constructor(private http: HttpClient) {
     this.http.get('http://localhost:8080/addtest').subscribe(() => {
@@ -23,10 +29,15 @@ export class ClairResultsComponent implements OnInit {
 
   ngOnInit() {
     this.httpGetClair().subscribe((data) => {
-      // data bestaat
-      console.log(data);
-      this.clairObject = data[0].genericReport.reports[0].genericResults;
+      // Add severity number so we can sort on this
+      data[0].vulnerabilities.forEach(function (vulnerability) {
+        let severityNumber = ClairResultsComponent.getSeverityNumber(vulnerability.severity);
+        vulnerability.severityNumber = severityNumber;
+      });
+
+      this.clairObject = data[0];
     });
+
     // Dropdown for option button in p-header
     this.items = [
       {
@@ -40,6 +51,12 @@ export class ClairResultsComponent implements OnInit {
         }
       }
     ];
+
+    // Sort options
+    this.sortOptions = [
+      {label: 'Severity High - Low', value: '!severityNumber'},
+      {label: 'Severity Low - High', value: 'severityNumber'}
+    ];
   }
 
   httpGetClair() {
@@ -50,9 +67,18 @@ export class ClairResultsComponent implements OnInit {
 
       })
     };
-    console.log(httpOption);
     return this.http.get('http://localhost:8080/xebia/vulnmanager/clair', httpOption);
   }
+
+  static getSeverityNumber = function (priority) {
+    return priority === 'Defcon1' ? 7
+      : priority === 'Critical' ? 6
+        : priority === 'High' ? 5
+          : priority === 'Medium' ? 4
+            : priority === 'Low' ? 3
+              : priority === 'Negligible' ? 2
+                : 1 // Unknown
+  };
 
   selectClair(event: Event, selectedClair: any) {
     this.selectedClair = selectedClair;
@@ -63,7 +89,17 @@ export class ClairResultsComponent implements OnInit {
 
   onDialogHide() {
     this.selectedClair = null;
-
   }
 
+  onSortChange(event) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
+  }
 }
