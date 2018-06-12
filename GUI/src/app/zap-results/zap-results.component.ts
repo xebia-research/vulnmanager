4e433416d4693807ae735dbf57df55c5ee29e228
Jelle Oppenhuis <jelle.oppenhuis@hva.nl>
2018-06-12 15:22:50 +0200

    #104 #107: Added siteselector, search and filter

--- GUI/src/app/zap-results/zap-results.component.ts
+++ GUI/src/app/zap-results/zap-results.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {MenuItem} from "primeng/api";
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MenuItem, SelectItem} from "primeng/api";
 import {VulnApiService} from "../services/vuln-api.service";
 
 @Component({
@@ -8,41 +8,81 @@ import {VulnApiService} from "../services/vuln-api.service";
   templateUrl: './zap-results.component.html',
   styleUrls: ['./zap-results.component.css']
 })
+
 export class ZapResultsComponent implements OnInit {
-  zapObject: any ;
+  zapObject: any;
   selectedZap: any;
+  currentSiteInformation: any;
   displayDialog: boolean;
   items: MenuItem[];
 
-  constructor(private http: HttpClient, private apiService:VulnApiService) {
-    this.apiService.addTest().subscribe(()=>{});
+  // Sort variables
+  sortOptions: SelectItem[];
+  sortKey: string;
+  sortField: string;
+  sortOrder: number;
+
+  constructor(private http: HttpClient, private apiService: VulnApiService) {
+    this.apiService.addTest().subscribe(() => {
+    });
 
   }
 
   ngOnInit() {
     this.apiService.getZap("xebia", "vulnmanager").subscribe((data) => {
       // data bestaat
-      console.log(data) ;
+      console.log(data);
+      let scannedSites = data[0].scannedSitesInformation;
+
+      this.currentSiteInformation = scannedSites[0];
       this.zapObject = data[0];
+      this.setItems(scannedSites);
+
+      // Sort options
+      this.sortOptions = [
+        {label: 'Severity High - Low', value: '!riskCode'},
+        {label: 'Severity Low - High', value: 'riskCode'}
+      ];
     });
-    // Dropdown for option button in p-header
-    this.items = [
-      {label: 'View scan info', icon: 'fa-eye', command: () => {
 
-        }},
-      {label: 'Delete', icon: 'fa-close', command: () => {
-
-        }}
-    ];
   }
+
+  setItems(scannedSiteInformation: any) {
+    let selectSiteItems = [];
+
+    for (let i = 0; i < scannedSiteInformation.length; i++) {
+      selectSiteItems.push({
+        label: scannedSiteInformation[i].host, icon: 'fa-eye', command: () => {
+          this.currentSiteInformation = scannedSiteInformation[i];
+        }
+      });
+    }
+
+    // Dropdown for option button in p-header
+    this.items = selectSiteItems;
+  }
+
   selectZap(event: Event, selectedZAP: any) {
     this.selectedZap = selectedZAP;
     this.displayDialog = true;
     event.preventDefault();
   }
+
   onDialogHide() {
     this.selectedZap = null;
 
   }
+
+  onSortChange(event) {
+    let value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
+    } else {
+      this.sortOrder = 1;
+      this.sortField = value;
+    }
   }
+}
 
