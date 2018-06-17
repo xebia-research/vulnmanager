06c7f602d2044c29a1e53fcb4c9d4dc818305d37
dbeltman <daan.w.beltman@gmail.com>
2018-06-18 00:14:49 +0200

    removed addcompany and addtest as they are in the home screen now

--- GUI/src/app/nav-bar/nav-bar.component.ts
+++ GUI/src/app/nav-bar/nav-bar.component.ts
@@ -78,20 +78,6 @@ export class NavBarComponent implements OnInit {
     ];
   }
 
-  runAddTest() {
-    this.apiService.addTest().subscribe(() => {
-      alert("The example logs are added, press OK to reload the page");
-      window.location.reload();
-    });
-  }
-
-  runAddCompany() {
-    this.apiService.addTestCompany().subscribe(() => {
-      alert("The example company was added, press OK to reload the page");
-      window.location.reload();
-    });
-  }
-
   runDelete() {
     let names:any = [];
     names[0] = "THE Dofensmithzinator";
