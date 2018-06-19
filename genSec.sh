b24999fe129841cc1cca7e34387f378efce29228
dbeltman <daan.w.beltman@gmail.com>
2018-06-19 03:12:44 +0200

    Made secret up to 256 standard with sha384sum

--- genSec.sh
+++ genSec.sh
@@ -1,5 +1,5 @@
 echo "Generating random string of 60 chars"
-RandomString=$(pwgen 60 1)
+RandomString=$(pwgen 60 1 | sha384sum | awk '{print $1}')
 
 echo "Random string was inserted to security constants!"
 sed -i "s/\<thisSecretNeedsToBeMovedToASecureLocation\>/$RandomString/g" src/main/java/com/xebia/vulnmanager/auth/security/SecurityConstants.java
